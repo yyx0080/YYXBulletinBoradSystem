133885281b8158a2e3a85f9f30714934b8d8f69c
yyx <273839986@qq.com>
2024-07-18 00:06:51 +0800

    修改了虚拟环境，同时增加了忽略文件，测试login

--- yyxbbs/Userlogin/views.py
+++ yyxbbs/Userlogin/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 # 写功能函数
 def index(request):
-    return HttpResponse("欢迎使用YYXBBS")
+    return HttpResponse("欢迎使用YYXBBS1.0")
+
+
+#登录函数
+def login(req):
+    #第一次访问，是get请求，所以这里把界面渲染给用户
+    if req.method == "GET":
+        return render(req,"login.html")
+    #否则为post请求，获取用户提交的数据
+    else:
+        print(req.POST)
+        return HttpResponse("登陆成功")
+    
